47e304b76fda10d33d158befab1562fb915c881b
Mitchell <mitchellhardie1@gmail.com>
2020-08-25 15:52:20 +0100

    added transmit_data() for general use

---
+++ src/server/utility.rs
@@ -0,0 +1,13 @@
+pub fn transmit_data(stream: &mut TcpStream, data: &str) -> Result<(), Error> {
+    println!("Transmitting...");
+    println!("data: {}", data);
+
+    /*
+     * This will throw an error and crash any thread, including the main thread, if
+     * the connection is lost before transmitting. Maybe change to handle any exceptions
+     * that may occur.
+     */
+    let _ = stream.write(data.to_string().as_bytes())?;
+    stream.flush()?;
+    Ok(())
+}
