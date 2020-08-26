28018f7ffaa8068a3897e26f633f8114aaaaa219
Mitchell <mitchellhardie1@gmail.com>
2020-08-26 12:58:11 +0100

    added all command functionality
    all the commands have now had there execution added into struct functions implement by traits. The general rule of thumb is, if the command is used by the server struct, it implements the ServerRunnables trait, and if the command is used by the client struct, it implements the ClientRunnables trait. This ensures each command can be used correctly.

--- src/commands/behaviors.rs
+++ src/commands/behaviors.rs
@@ -10,11 +10,9 @@ struct Info {
 
 struct Connect {
     command: Commands,
-    params: Option<HashMap<String, String>>,
 }
 
 struct Disconnect {
-    command: Commands,
 }
 
 struct ClientUpdate {
@@ -22,15 +20,17 @@ struct ClientUpdate {
 }
 
 struct ClientInfo {
-    command: Commands,
+    uuid: String,
 }
 
 struct ClientRemove {
     command: Commands,
+    error: Commands,
 }
 
 struct Client {
     command: Commands,
+    error: Commands,
 }
 
 struct Success {
@@ -42,20 +42,20 @@ struct Error {
 }
 
 trait ServerRunnables {
-    fn execute(&self, stream: &TcpStream server: &Server);
+    fn execute(&self, stream: &TcpStream, server: &Server);
 }
 
 trait ClientRunnables {
-    fn execute(&self, stream: &TcpStream client: &Client);
+    fn execute(&self, stream: &TcpStream, client: &Client);
     
-    fn execute(&self, stream: &TcpStream _: &Server) {
+    fn execute(&self, stream: &TcpStream, _server: &Server) {
         println!("Server: Invalid command sent");
         utility::transmit_data(stream, Commands::Error(None).to_string().as_str());
     }
 }
 
 impl ServerRunnables for Info {
-    fn run(&self, stream: &TcpStream _: &Server) {
+    fn run(&self, stream: &TcpStream, _server: &Server) {
         println!("Server: info requested");
 
         utility::transmit_data(stream, self.command.to_string().as_str());
@@ -64,10 +64,6 @@ impl ServerRunnables for Info {
 
 impl ServerRunnables for Connect {
     fn run(&self, stream: &TcpStream, server: &Server) {
-        let uuid = self.params.get("uuid").unwrap();
-        let username = self.params.get("name").unwrap();
-        let address = self.params.get("host").unwrap();
-        
         println!("{}", format!("Server: new Client connection: _addr = {}", address ));
         
         let client = Client::new(stream, server.get_sender().clone(), &uuid, &username, &address);
@@ -75,10 +71,7 @@ impl ServerRunnables for Connect {
         let connected_clients = server.get_connected_clients();
         connected_clients.lock().unwrap().insert(uuid.to_string(), client);
         
-        let params: HashMap<String, String> = [(String::from("name"), username.clone()), (String::from("host"), address.clone()), (String::from("uuid"), uuid.clone())].iter().cloned().collect();
-        let new_client = Commands::Client(Some(params));
-        
-        let _ = connected_clients.lock().unwrap().iter().map(|(_k, v)| v.sender.send(new_client.clone()));
+        let _ = connected_clients.lock().unwrap().iter().map(|(_k, v)| v.sender.send(self.command.clone()));
     }
 }
 
@@ -88,25 +81,62 @@ impl ServerRunnables for Connect {
 
 
 impl ClientRunnables for Disconnect {
-    fn run() {
+    fn execute(&self, _stream: &TcpStream, client: &Client) {
+        client.get_server_sender().send(ServerMessages::Disconnect(client.get_uuid())).expect("sending message to server failed");
+        client.get_stream_arc().lock().unwrap().shutdown(Shutdown::Both).expect("shutdown call failed");
     }
 }
 
 impl ClientRunnables for ClientUpdate {
+    fn execute(&self, stream: &TcpStream, client: &Client) {
+        utility::transmit_data(stream, self.command.to_string().as_str());
+        let _ = client.get_server_sender().send(ServerMessages::RequestUpdate(client.get_stream_arc().clone()));
+    }
 }
 
 impl ClientRunnables for ClientInfo {
-    fn run() {
+    fn execute(&self, stream: &TcpStream, client: &Client) {
+        let _ = client.get_server_sender().send(ServerMessages::RequestInfo(self.uuid.clone(), client.get_stream_arc().clone()));
     }
 }
 
 impl ClientRunnables for ClientRemove {
-    fn run() {
+    fn execute(&self, stream: &TcpStream, buffer: &mut [u8; 1024]) {
+        let mut retry: u8 = 3;
+        'retry_loop: loop {
+            if retry < 1 {
+                utility::transmit_data(self.error.to_string().as_str());
+                break 'retry_loop;
+            } else {                    
+                utility::transmit_data(self.command.to_string().as_str());
+
+                if utility::read_data(buffer).unwrap_or(Commands::Error(None)) == Commands::Success(None) {
+                    break 'retry_loop;
+                } else {
+                    retry -= 1;
+                }
+            }
+        }
     }
 }
 
 impl ClientRunnables for Client {
-    fn run() {
+    fn execute(&self, stream: &TcpStream, buffer: &mut [u8; 1024]) {
+        let mut retry: u8 = 3;
+        'retry_loop: loop {
+            if retry < 1 {
+                utility::transmit_data(self.error.to_string().as_str());
+                break 'retry_loop;
+            } else {
+                utility::transmit_data(self.command.to_string().as_str());
+                
+                if utility::read_data(buffer).unwrap_or(Commands::Error(None)) == Commands::Success(None) {
+                    break 'retry_loop;
+                } else {
+                    retry -= 1;
+                }
+            }
+        }
     }
 }
 
