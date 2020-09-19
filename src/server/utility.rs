0a5ed56b867f0adf38245fefbd9fab9cff1c8ddf
Mitchell <mitchellhardie1@gmail.com>
2020-09-19 14:06:38 +0100

    added comment about the result and io error

--- src/server/utility.rs
+++ src/server/utility.rs
@@ -6,6 +6,12 @@ use std::io::Write;
 pub fn transmit_data(stream: &mut TcpStream, data: &str) -> Result<(), Error> {
     println!("Transmitting...");
     println!("data: {}", data);
+    
+    /* TODO
+     * need some sort of handling for io erros if data fails to write to the stream,
+     * currently all if a error happens, its stored in '_', ignoring it.
+     */
+
 
     /*
      * This will throw an error and crash any thread, including the main thread, if
